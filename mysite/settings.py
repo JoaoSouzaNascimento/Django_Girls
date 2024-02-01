4bd0c7dd2b6da6c6129501a4c04c1d2c81f64451
jvsn <jvsn.10.jv@gmail.com>
2024-01-31 21:47:58 -0300

    Tutorial Django Girls, mudando para o banco de dados PostgreSQL.

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'joao_victor',
+        'PASSWORD': 'joao_victorpassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
